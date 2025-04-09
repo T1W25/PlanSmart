1be316889442b9a3d83e4a62e1f058ee523c209b
Darkest <75559042+3th34-bit@users.noreply.github.com>
2025-04-09 14:09:28 -0700

    another change to server.js capitlization issues

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.post("/api/upload", upload.single("media"), (req, res) => {
 
 // Route imports
 const guestSpeakerRoutes = require('./routes/guestSpeakerRoutes');
-const vendorRoutes = require('./routes/VendorRoutes');
+const vendorRoutes = require('./routes/vendorRoutes');
 const transportationProviderRoutes = require('./routes/transportationProviderRoutes');
 const portfolioRoutes = require('./routes/PortfolioRoutes');
 const organizationRoutes = require('./routes/OrganizationRoutes');
